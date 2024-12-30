503f57d9d3c7a424468c6b2a4f7afee151efbd07
manu-xo5 <mohit.matwaya@gmail.com>
2024-12-31 00:19:50 +0530

    feat: add Theme colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,22 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    bg: "#fff",
+    bgSecondary: "#f3f3f3",
+    bgTertiary: "#dddde1",
+    fg: "#000",
+    fgSecondary: "#8a8a8e",
+    fgTertiary: "#f0eff5",
+    accentPrimary: "#007dfe",
+    accentDestructive: "#ff0000",
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    bg: "#fff",
+    bgSecondary: "#f3f3f3",
+    bgTertiary: "#dddde1",
+    fg: "#000",
+    fgSecondary: "#8a8a8e",
+    fgTertiary: "#f0eff5",
+    accentPrimary: "#007dfe",
+    accentDestructive: "#ff0000",
   },
 };
